acd0ddb1715594c27dee52cb7756f845ae531d96
bvanberl <bvanberl@uwo.ca>
2021-04-11 22:29:24 -0400

    First custom CNN

--- src/models/models.py
+++ src/models/models.py
@@ -1,6 +1,7 @@
 import tensorflow as tf 
 from tensorflow.keras import Model
-from tensorflow.keras.layers import Dense, Dropout, Input, Activation, GlobalAveragePooling2D
+from tensorflow.keras.layers import Dense, Dropout, Input, Activation, GlobalAveragePooling2D, Conv2D, MaxPool2D, \
+    BatchNormalization, ZeroPadding2D
 from tensorflow.keras.regularizers import l2
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.initializers import Constant
@@ -28,9 +29,12 @@ def get_model(model_name):
     elif model_name == 'mobilenetv2':
         model_def = mobilenetv2
         preprocessing_function = mobilenetv2_preprocess
-    else:
+    elif model_name == 'xception':
         model_def = xception
         preprocessing_function = xception_preprocess
+    else:
+        model_def = cnn0
+        preprocessing_function = mobilenetv2_preprocess
     return model_def, preprocessing_function
 
 def mobilenetv2(model_config, input_shape, metrics, n_classes, mixed_precision=False, output_bias=None):
@@ -228,6 +232,55 @@ def efficientnetb7(model_config, input_shape, metrics, n_classes, mixed_precisio
     X = GlobalAveragePooling2D()(X)
     X = Dropout(dropout)(X)
     X = Dense(n_classes, bias_initializer=output_bias, name='logits')(X)
+    Y = Activation('softmax', dtype='float32', name='output')(X)
+
+    # Set model loss function, optimizer, metrics.
+    model = Model(inputs=X_input, outputs=Y)
+    model.summary()
+    model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=metrics)
+    return model
+
+
+def cnn0(model_config, input_shape, metrics, n_classes, mixed_precision=False, output_bias=None):
+    # Set hyperparameters
+    nodes_dense0 = model_config['NODES_DENSE0']
+    lr = model_config['LR']
+    dropout = model_config['DROPOUT']
+    l2_lambda = model_config['L2_LAMBDA']
+    optimizer = Adam(learning_rate=lr)
+    init_filters = model_config['INIT_FILTERS']
+    filter_exp_base = model_config['FILTER_EXP_BASE']
+    n_blocks = model_config['BLOCKS']
+    kernel_size = eval(model_config['KERNEL_SIZE'])
+    max_pool_size = eval(model_config['MAXPOOL_SIZE'])
+    strides = eval(model_config['STRIDES'])
+    pad = kernel_size[0] // 2
+    print("MODEL CONFIG: ", model_config)
+    if mixed_precision:
+        tf.train.experimental.enable_mixed_precision_graph_rewrite(optimizer)
+
+    if output_bias is not None:
+        output_bias = Constant(output_bias)  # Set initial output bias
+
+    # Input layer
+    X_input = Input(input_shape)
+    X = X_input
+    X = ZeroPadding2D((pad, pad))(X)
+
+    # Add blocks of convolutions and max pooling
+    for i in range(n_blocks):
+        filters = init_filters * (2 ** i)
+        X = Conv2D(filters=filters, kernel_size=kernel_size, strides=strides, padding='same', name='conv2d_block' + str(i) + '_0',
+                   kernel_initializer='he_uniform', activation='relu', activity_regularizer=l2(l2_lambda))(X)
+        X = BatchNormalization(axis=3, name='bn_block' + str(i))(X)
+        if i < n_blocks - 1:
+            X = MaxPool2D(max_pool_size, padding='same', name='maxpool' + str(i))(X)
+
+    # Model head
+    X = GlobalAveragePooling2D(name='global_avgpool')(X)
+    X = Dropout(dropout)(X)
+    X = Dense(nodes_dense0, kernel_initializer='he_uniform', activity_regularizer=l2(l2_lambda), activation='relu', name='fc0')(X)
+    X = Dense(n_classes, bias_initializer=output_bias)(X)
     Y = Activation('softmax', dtype='float32', name='output')(X)
 
     # Set model loss function, optimizer, metrics.
